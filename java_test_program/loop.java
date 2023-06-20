3e92e0e2b481565ad552aae3d0a38956f166b53a
nazmulislamprofile <74948943+nazmulislamprofile@users.noreply.github.com>
2023-06-20 08:54:33 +0000

    learning java input

--- java_test_program/loop.java
+++ java_test_program/loop.java
@@ -1,8 +1,21 @@
+
+import java.util.Scanner;
+
 class Loop{
     public static void main(String[] args){
-        for(int i=0; i<10; i++){
-            System.out.println("hello world");
+        System.out.println("please entered your desired number");
+        Scanner input=new Scanner(System.in);
+        int number,sum=0;
+    for(int i=0; i<5; i++){
+        number=input.nextInt();
+        if(number>5&&number<20){
+            continue;
         }
-            
+        sum+=number;
+       
+    }  
+     System.out.println("your entered qty sumession"+sum);
+     input.close();  
+    
     }
 }
